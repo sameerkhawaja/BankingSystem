c0b1ef2456a954c7442af29b842884906c2c80ec
sameerkhawaja <sameerkhawaja01@gmail.com>
2023-08-31 15:39:32 -0400

    removed unused user service and user model

--- BankingSystem.API/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BankingSolution.API.Models
-{
-    public class User
-    {
-        public Guid Id { get; set; } = Guid.NewGuid();
-        public string Name { get; set; }
-        public List<Account> Accounts { get; set; } = new List<Account>();
-    }
-}
--- BankingSystem.API/Program.cs
+++ BankingSystem.API/Program.cs
@@ -9,7 +9,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddTransient<IUserService, UserService>();
 builder.Services.AddTransient<IAccountService, AccountService>();
 builder.Services.AddSingleton<IMockRepository, MockRepository>();
 
